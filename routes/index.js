5bba1c79d521c0495e886f49a76559086bd81e1b
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-10-06 16:47:28 -0300

    PDV

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 module.exports = app => {
   const controller = app.controllers;
+  const path = require('path');
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'home.html'));
+  });
 
   app.get('/documentation');
+
    /**
    * @swagger
    * definition:
