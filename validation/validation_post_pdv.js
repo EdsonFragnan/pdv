a78d8de6ef2b315d8e5346a2cf6c38825f25de52
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-10-09 00:25:14 -0300

    PDV

--- validation/validation_post_pdv.js
+++ validation/validation_post_pdv.js
@@ -2,35 +2,34 @@
 const CNPJ = require("cpf_cnpj").CNPJ;
 module.exports.validation_post_pdv = (body, res) => {
     const controller_postpdv = require('../controllers/pdv_post_pdv_controller');
-    switch(body) {
-      case (body.id === '' || body.id === null || body.id === undefined):
-          res.status(422).json({mensagem: 'Invalid PDV ID.'});
-          break;
-      case (body.tradingName === '' || body.tradingName === null || body.tradingName === undefined):
-          res.status(422).json({mensagem: 'Invalid Trading Name.'});
-          break;
-      case (body.ownerName === '' || body.ownerName === null || body.ownerName === undefined):
-          res.status(422).json({mensagem: 'Invalid Owner Name.'});
-          break;
-      case (body.document === '' || body.document === null || body.document === undefined):
-          res.status(422).json({mensagem: 'Invalid Document.'});
-          break;
-      case (CNPJ.isValid(body.document) != true):
-          res.status(422).json({mensagem: 'Invalid Document.'});
-          break;
-      case (body.lng === '' || body.lng === null || body.lng === undefined):
-          res.status(422).json({mensagem: 'Invalid LNG.'});
-          break;
-      case (body.lat === '' || body.lat === null || body.lat === undefined):
-          res.status(422).json({mensagem: 'Invalid LAT.'});
-          break;
-      case (body.distanceType === '' || body.distanceType === null || body.distanceType === undefined):
-          res.status(422).json({mensagem: 'Invalid Type Distance.'});
-          break;
-      case (body.pointDistance === '' || body.pointDistance === null || body.pointDistance === undefined):
-          res.status(422).json({mensagem: 'Invalid Point Distance.'});
-          break;
-      default:
-          controller_postpdv.postPDV(body, res);
+    if (body.id === '' || body.id === null || body.id === undefined) {
+      res.status(422);
+      res.json({mensagem: 'Invalid PDV ID.'});
+    } else if (body.tradingName === '' || body.tradingName === null || body.tradingName === undefined) {
+      res.status(422);
+      res.json({mensagem: 'Invalid Trading Name.'});
+    } else if (body.ownerName === '' || body.ownerName === null || body.ownerName === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid Owner Name.'});
+    } else if (CNPJ.isValid(body.document) === false) {
+        res.status(422);
+        res.json({mensagem: 'Invalid Document.'});
+    } else if (body.document === '' || body.document === null || body.document === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid Document.'});
+    } else if (body.lng === '' || body.lng === null || body.lng === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid LNG.'});
+    } else if (body.lat === '' || body.lat === null || body.lat === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid LAT.'});
+    } else if (body.distanceType === '' || body.distanceType === null || body.distanceType === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid Type Distance.'});
+    } else if (body.pointDistance === '' || body.pointDistance === null || body.pointDistance === undefined) {
+        res.status(422);
+        res.json({mensagem: 'Invalid Point Distance.'});
+    } else {
+      controller_postpdv.postPDV(body, res);
     }
 };
