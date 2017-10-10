6d75fa739820887d15cf8495dc4754aee61d65f4
Edson Luiz Fragnan <edsonfragnan@brq.com>
2017-10-10 17:39:56 -0300

    PDV

--- controllers/pdv_post_pdv_controller.js
+++ controllers/pdv_post_pdv_controller.js
@@ -1,44 +1,60 @@
 'use strict';
 
 module.exports.postPDV = (pdv, res) => {
-  const PDV = require('../models/pdv.js');
+  const GeoJSON = require('geojson');
   const PolygonCoordinates = require('polygon-coordinates');
-  const Polygon = PolygonCoordinates.polygonCoordinates(pdv.lat, pdv.lng, 22, pdv.pointDistance);
-  const MultiPolygon = [];
-  MultiPolygon.push(Polygon);
-  const object = {
-    'id': pdv.id,
-    'tradingName': pdv.tradingName,
-    'ownerName': pdv.ownerName,
-    'document': pdv.document,
-    'coverageArea': {
-      'type': 'MultiPolygon',
-      'coordinates': [MultiPolygon]
-    },
-    'address': {
-      'type': 'Point',
-      'coordinates': [pdv.lat, pdv.lng]
-    }
+  const TurfMultiPolygon = require('turf-multipolygon');
+  const ModelPdv = require('../models/pdv.js');
+
+  const TreatObject = (point_pdv) => {
+    let object = {
+      'id': point_pdv.properties.id,
+      'tradingName': point_pdv.properties.tradingName,
+      'ownerName': point_pdv.properties.ownerName,
+      'document': point_pdv.properties.document,
+      'coverageArea': {
+        'type': point_pdv.multi_polygon_object.geometry.type,
+        'coordinates': point_pdv.multi_polygon_object.geometry.coordinates
+      },
+      'address': {
+        'type': point_pdv.geometry.type,
+        'coordinates': point_pdv.geometry.coordinates
+      }
+    };
+    return object;
   };
-  PDV.postPDV(object, (err, data) => {
-    if (err) {
-      res.status(422).json({mensagem: 'PDV not registered.'});
-    } else {
-      const response = {
-        'id': data.id,
-        'tradingName': data.tradingName,
-        'ownerName': data.ownerName,
-        'document': data.document,
-        'coverageArea': {
-          'type': data.coverageArea.type,
-          'coordinates': data.coverageArea.coordinates,
-        },
-        'address': {
-          'type': data.address.type,
-          'coordinates': data.address.coordinates
-        }
-      };
-      res.json(response);
-    }
-  });
+
+  const InserPdv = (object) => {
+    ModelPdv.postPDV(object, (err, data) => {
+      if (err) {
+        res.status(422).json({mensagem: 'PDV not registered.'});
+      } else {
+        res.json(data);
+      }
+    });
+  };
+
+  const MultiPolygon = (point_pdv) => {
+    let multi_polygon_object = TurfMultiPolygon(point_pdv.polygon_pdv);
+    point_pdv.multi_polygon_object = multi_polygon_object;
+    let treat_object = TreatObject(point_pdv);
+    InserPdv(treat_object);
+  };
+
+  const Polygon = (point_pdv) => {
+    let polygon_pdv = PolygonCoordinates.polygonCoordinates(
+                        point_pdv.geometry.coordinates[0],
+                        point_pdv.geometry.coordinates[1],
+                        22, point_pdv.properties.pointDistance
+                      );
+    point_pdv.polygon_pdv = polygon_pdv;
+    MultiPolygon(point_pdv);
+  };
+
+  const Point = (pdv) => {
+    let point_pdv = GeoJSON.parse(pdv, {Point: ['lat', 'lng']});
+    Polygon(point_pdv)
+  };
+
+  Point(pdv);
 };
